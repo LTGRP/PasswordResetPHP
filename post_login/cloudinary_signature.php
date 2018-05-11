46f2106f88d7adccab93336885b3db14735bd082
Tami Williams <tami@asktami.com>
2018-05-11 04:49:54 -0400

    removed server specific info

--- post_login/cloudinary_signature.php
+++ post_login/cloudinary_signature.php
@@ -5,6 +5,6 @@ include_once('../cloudinary_settings.php');
 
     if(isset($_GET['data']))
     {
-        echo \Cloudinary::api_sign_request($_GET['data'], 'EkGD-AZvVIy9wl7KnfB_3QgffVk'); // CLOUDINARY API SECRET
+        echo \Cloudinary::api_sign_request($_GET['data'], 'YOUR CLOUDINARY API SECRET');
     }
 ?>
